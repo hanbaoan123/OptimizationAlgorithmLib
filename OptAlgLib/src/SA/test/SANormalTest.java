ab432bde6d4e25a0aa9d39f1f39f839ffdf064ac
hanbaoan123 <hanbaoan@buaa.edu.cn>
2020-04-10 08:57:54 +0800

    SA

--- OptAlgLib/src/SA/test/SANormalTest.java
+++ OptAlgLib/src/SA/test/SANormalTest.java
@@ -6,7 +6,7 @@ import SA.algorithm.SANormal;
 public class SANormalTest {
 	public static void main(String[] args) {
 		ObjectiveFun objectiveFun = new ObjectiveFun();
-		SANormal sa = SANormal.builder().maxGen(200).dim(2).T0(10).beta(10).objectiveFun(objectiveFun).build();
+		SANormal sa = SANormal.builder().maxGen(400).dim(2).T0(10).beta(200).objectiveFun(objectiveFun).build();
 		sa.start();
 	}
 }
